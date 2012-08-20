c3c1e4a0be0ceb33d9f66773e3b7e7e3a22e7984
Mikhail <Mikhail.Kapusta@jetbrains.com>
2012-08-20 18:05:19 +0400

    test commit message <script>alert('xxxx')</script> #GHS-4 Fixed Critical Task xxx add tag BBBBB

--- test.py
+++ test.py
@@ -5,41 +5,13 @@ wr
 rew
 ere
 wrrrrrrrrwer
-wer
+werdsfsdsdffdsdf
 we
 rasasdasdasdsdaasd
 zxc
 xzc
 xcz
-zxczcx
-sdfsdfsd
-we
-r
-wer
-we
-we
-r
-wer
-werwtyuuytytufsdfsdfs
-ytuasdassdaaaaaaaaaa
-
-asdasdasd
-sda
-sda
-uytyfsdfsdf
-tu
-uyt
-yut
-yut
-ytu
-uty
+fsdf
 sdf
-Are you logged in with same user?
-dsffsd
-and you can change password.
-
-
-1. You can merge root with any user. ()
-But you can't merge user with root.
-
-
+sdf
+dff
